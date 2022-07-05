a21e769558ab6ecd8a6fcff96090b577bfb6e583
luimen11 <2280512150101@ingenieria.usac.edu.gt>
2022-07-04 23:47:30 -0600

    clasificador gausiano y arboles de decision

--- main.py
+++ main.py
@@ -8,13 +8,23 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn import preprocessing, tree
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 
+
+#st.beta_set_page_config(page_title='Segundo Proyecto')
 #======================================================================
 # CARGAR ARCHIVO
 #======================================================================
 archivo = st.sidebar.file_uploader("Seleccione un archivo", type=['csv', 'xls', 'xlsx', 'json'])
+
+if archivo is None:
+     st.title('Compiladores 2')
+     st.header('Segundo proyecto')
+     st.subheader('Curso de vacaciones junio 2022')
 #Se obtiene la extension del archivo
-if archivo:
+elif archivo:
      archivo_split = os.path.splitext(archivo.name)
      nombre_archivo = archivo_split[0]
      ext_archivo = archivo_split[1]
@@ -36,7 +46,7 @@ if archivo:
      #======================================================================
      # SE ELIGE EL AGORITMO A UTILIZAR
      #======================================================================          
-     algoritmos = ["Regresion lineal","Regresion polinomial", "Clasificador gaussiano"]
+     algoritmos = ["Seleccione...","Regresion lineal","Regresion polinomial", "Clasificador gaussiano", "Clasificador de arboles de decision", "Redes neuronales"]
      opcion = st.selectbox("Seleccione algoritmo", algoritmos)
      encabezados = df.columns.values.tolist()
 
@@ -93,54 +103,138 @@ if archivo:
 
           nb_degree = st.number_input('Ingrese el grado: ')   
           nb_degree = int(nb_degree)
-          polynomial_features = PolynomialFeatures(degree = nb_degree)
+          if nb_degree != 0:               
+               polynomial_features = PolynomialFeatures(degree = nb_degree)
 
-          x_train_pol = polynomial_features.fit_transform(x_train)
-          x_test_pol = polynomial_features.fit_transform(x_test)
+               x_train_pol = polynomial_features.fit_transform(x_train)
+               x_test_pol = polynomial_features.fit_transform(x_test)
 
-          #Defino el algoritmo
-          model = linear_model.LinearRegression()
+               #Defino el algoritmo
+               model = linear_model.LinearRegression()
 
-          #Entreno el modelo
-          model.fit(x_train_pol, y_train)
+               #Entreno el modelo
+               model.fit(x_train_pol, y_train)
 
-          #Prediccion
-          y_pred = model.predict(x_test_pol)
+               #Prediccion
+               y_pred = model.predict(x_test_pol)
 
-          st.subheader('Resultados:')
+               st.subheader('Resultados:')
 
-          #Graficamos los datos junto con el modelo
-          fig1, ax1 = plt.subplots()
-          plt.scatter(x_test, y_test)
-          plt.plot(x_test, y_pred, color='red', linewidth=3)
-          st.pyplot(fig1)
+               #Graficamos los datos junto con el modelo
+               fig1, ax1 = plt.subplots()
+               plt.scatter(x_test, y_test)
+               plt.plot(x_test, y_pred, color='red', linewidth=3)
+               st.pyplot(fig1)
 
-          #Datos del modelo
-          st.write('Valor de la pendiente o coeficiente a')
-          st.write(model.coef_)
+               #Datos del modelo
+               st.write('Valor de la pendiente o coeficiente a')
+               st.write(model.coef_)
 
-          st.write('Valor de la inteserccion o coeficiente b')
-          st.write(model.intercept_)
-          
-          st.metric(label='Precision del modelo', value=(model.score(x_train_pol, y_train)))
-          #st.write(model.score(x_train_pol, y_train))
-           
-          rmse = np.sqrt(mean_squared_error(y_test, y_pred))                   
-          st.metric(label='Error cuadrático medio: ', value=rmse)
-          
-          r2 = r2_score(y_test, y_pred)          
-          st.metric(label='Varianza: ', value=r2)
+               st.write('Valor de la inteserccion o coeficiente b')
+               st.write(model.intercept_)
+               
+               st.metric(label='Precision del modelo', value=(model.score(x_train_pol, y_train)))
+               #st.write(model.score(x_train_pol, y_train))
+               
+               rmse = np.sqrt(mean_squared_error(y_test, y_pred))                   
+               st.metric(label='Error cuadrático medio: ', value=rmse)
+               
+               r2 = r2_score(y_test, y_pred)          
+               st.metric(label='Varianza: ', value=r2)
      
-          #Prediccion
-          st.subheader('Prediccion:')          
-          pred2 = st.number_input('Ingrse el número de la predicción: ')
-
-          st.write(model.predict(polynomial_features.fit_transform([[int(pred2)]])))
+               #Prediccion
+               st.subheader('Prediccion:')          
+               pred2 = st.number_input('Ingrse el número de la predicción: ')
+               pred2 = int(pred2)
+               if pred2 != 0:
+                    st.write(model.predict(polynomial_features.fit_transform([[pred2]])))
           
      #======================================================================
-     # REGRESION POLINOMIAL
+     # Clasificador gaussiano
      #======================================================================          
-     #elif opcion == 'Clasificador gaussiano':
+     elif opcion == 'Clasificador gaussiano':
+          st.write(df) 
+          le = preprocessing.LabelEncoder()
+          encabezados = df.columns.values.tolist()
+          dic_enc = {}
+          print(encabezados)
+          last = encabezados.pop()
+          for a in encabezados:
+               if(a!='#' and a!='NO' and a!='No.' and a!='NO.' and a!='Num'and a!=last):
+                    temp = np.asarray(df[a])    
+                    dic_enc[a] = le.fit_transform(temp)
+          
+          play = np.asarray(df[last])
+          label = le.fit_transform(play)
+          features = list(zip(*dic_enc.values()))
+          #print(features)
+          st.write(features)
+          st.table(features)
+          model = GaussianNB()
+
+          model.fit(features, label)
+
+          valores = st.text_input("Ingrese los valores a predecir seprados por comas:")
+
+          if(valores != ''):
+               valor_num = valores.split(',')
+               lista = []
+               for a in valor_num:
+                    lista.append(int(a))
+
+               #predicted = model.predict([[2,1,1,0]])          
+               predicted = model.predict([lista])          
+               st.metric(label='Prediccion ', value=predicted)
+
+     #======================================================================
+     # Clasificador de arboles de decision
+     #======================================================================  
+     elif opcion == "Clasificador de arboles de decision":
+          st.write(df) 
+          le = preprocessing.LabelEncoder()
+          encabezados = df.columns.values.tolist()
+          dic_enc = {}
+          print(encabezados)
+          last = encabezados.pop()
+          for a in encabezados:
+               if(a!='#' and a!='NO' and a!='No.' and a!='NO.' and a!='Num'and a!=last):
+                    temp = np.asarray(df[a])    
+                    dic_enc[a] = le.fit_transform(temp)
+          
+          play = np.asarray(df[last])
+          label = le.fit_transform(play)
+          features = list(zip(*dic_enc.values()))
+          
+          st.write(features)
+          st.table(features)
+          
+          clf = DecisionTreeClassifier()
+          
+          clf.fit(features, label)
+
+
+          target = list(df[last].unique())
+          fig1, ax1 = plt.subplots()
+          plot_tree(clf, filled=True, class_names=target)
+          #plt.show()
+          st.pyplot(fig1)
+              
+          valores = st.text_input("Ingrese los valores a predecir seprados por comas:")
+
+          if(valores != ''):
+               valor_num = valores.split(',')
+               lista = []
+               for a in valor_num:
+                    lista.append(int(a))
+               
+               predicted = clf.predict([lista])          
+               st.metric(label='Prediccion ', value=predicted)
+
+     #======================================================================
+     # Redes neuronales
+     #======================================================================  
+     elif opcion == "Redes neuronales":
+          pass
 
 
 
